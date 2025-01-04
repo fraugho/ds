85a81586dddd29ad9885d03f6685428afd3f392d
Agho Fru <black@noir.local>
2025-01-04 14:09:35 -0900

    added vec insert

--- vec/main.c
+++ vec/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,6 +24,29 @@ vec_append(struct Vec *vec, void* thing){
     }
 }
 
+void
+vec_pop(struct Vec *vec, void* dest){
+    if (vec->used | 0){
+        memcpy(dest, &vec->data[--vec->used * vec->type_size], vec->type_size);
+    }
+}
+
+void
+vec_insert(struct Vec *vec, void* thing, int32_t index){
+    if (vec->used < vec->capacity){
+        memcpy(&vec->data[(index + 1) * vec->type_size], &vec->data[index * vec->type_size], vec->type_size *  (vec->used - index));
+        memcpy(&vec->data[index * vec->type_size], thing, vec->type_size);
+        ++vec->used;
+    }
+    else{
+        vec->data = realloc(vec->data, vec->capacity * vec->type_size * 2);
+        memcpy(&vec->data[(index + 1) * vec->type_size], &vec->data[index * vec->type_size], vec->type_size *  (vec->used - index));
+        memcpy(&vec->data[index * vec->type_size], thing, vec->type_size);
+        ++vec->used;
+        vec->capacity *= 2;
+    }
+}
+
 int
 main(){
     int cap = 10;
@@ -34,12 +58,22 @@ main(){
         printf("%d\n", *((int*)vec.data + i));
     }
 
-    for (int i = cap; i < cap + cap; ++i){
+    printf("append\n");
+    for (int i = cap; i < cap + cap - 1; ++i){
         vec_append(&vec, &i);
     }
-    printf("double\n");
     for (int i = 0; i < vec.capacity; ++i){
         printf("%d\n", *((int*)vec.data + i));
     }
+
+    printf("insert\n");
+
+    int x = 44;
+    vec_insert(&vec, &x, 9);
+
+    for (int i = 0; i < vec.capacity; ++i){
+        printf("%d\n", *((int*)vec.data + i));
+    }
+
     free(vec.data);
 }
