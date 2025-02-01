46ce24af414e68d46e0cee006b3f82fdcd565058
Agho Fru <black@noir.local>
2025-01-31 21:26:16 -0900

    cleaning up

--- arena/main.c
+++ arena/main.c
@@ -153,36 +153,3 @@ void time_test(uint32_t times, uint32_t quant){
 int main(){
     time_test(100000, 100);
 }
-
-/*  
-    int num = 10;
-    Arena* arena = arena_new(num * sizeof(Monster));
-    Monster* m_1 = arena_alloc(arena, num * sizeof(Monster));
-    for(int i = 0; i < num; ++i){
-        m_1[i].atk = i;
-        m_1[i].hp = i + 1;
-    }
-
-    print_monster(m_1, num);
-
-    free(arena);
-
-    MultiPool* mp = multi_pool_new(num * sizeof(Monster));
-    Monster* m_2 = mp_arena_alloc(mp, num * sizeof(Monster));
-    for(int i = 0; i < num; ++i){
-        m_2[i].atk = i;
-        m_2[i].hp = i + 1;
-    }
-
-    print_monster(m_2, num);
-
-    Monster* m_3 = mp_arena_alloc(mp, num * sizeof(Monster));
-    for(int i = 0; i < num; ++i){
-        m_3[i].atk = i * 2;
-        m_3[i].hp = (i + 1) * 2;
-    }
-
-    print_monster(m_3, num);
-    free(mp);
-
-*/
